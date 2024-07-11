ca6e26ff88d62cf739758ede17558e524849f9e5
Benjamin Schwehn <bschwehn@gmail.com>
2024-07-11 20:13:51 +0200

    Test for performance issue

---
+++ TestRun/PerfIssue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Json.Schema;
+
+namespace TestRun;
+
+public static class PerfIssueTest
+{
+	private static string GetFile(string issue, string name)
+	{
+		return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files", $"Issue{issue}_{name}.json");
+	}
+
+	private static string GetResource(string issue, string name)
+	{
+		return File.ReadAllText(GetFile(issue, name));
+	}
+
+	public static void PerfIssueReduced()
+	{
+		var schemaText = GetResource("Perf", "schemareduced4");
+		var data = GetResource("Perf", "datareduced4");
+
+		var schema = JsonSchema.FromText(schemaText);
+
+		// SchemaRegistry.Global.Fetch = RegistryFetch;
+		JsonDocumentOptions options = new()
+		{
+			AllowTrailingCommas = true,
+			CommentHandling = JsonCommentHandling.Skip
+		};
+
+		var testDoc = JsonDocument.Parse(data, options);
+		var validationResult = schema.Evaluate(testDoc, new EvaluationOptions()
+		{
+			OutputFormat = OutputFormat.Flag
+		});
+
+		if (!validationResult.IsValid)
+		{
+			Console.WriteLine("Schema validation failed.");
+		}
+	}
+}
---
+++ TestRun/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace TestRun
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+			Console.WriteLine("start reduced3");
+			PerfIssueTest.PerfIssueReduced();
+			sw.Stop();
+			Console.WriteLine($"Took {sw.Elapsed.TotalSeconds}");
+			Console.ReadLine();
+		}
+	}
+}
